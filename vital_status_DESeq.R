654a5f6fcea61ccc0ce23643daf1a50314477135
Matt-Preece <mdpreece1@hotmail.com>
2025-07-17 11:48:19 +0100

    Update code
    Cleaned and made more efficient

--- vital_status_DESeq.R
+++ vital_status_DESeq.R
@@ -1,8 +1,9 @@
-#setwd("/data/home/qp241207/TCGA_query/raw_data/")
 setwd("G:/Desktop/Corr. Download")
+cancer <- "BRCA"
+goi <- c("ATAT1","HDAC6","SIRT2")
+goi <- toupper(goi)
 
 libraries <- c("TCGAbiolinks","SummarizedExperiment","dplyr","ggplot2","ggpubr","cowplot","rstatix","DESeq2","BiocParallel","parallel")
-
 for (lib in libraries) {
   if (require(package = lib, character.only = TRUE)) {
     successful <- "Successful"
@@ -16,9 +17,7 @@ for (lib in libraries) {
 }
 
 
-cancer <- "BRCA"
-goi <- c("ATAT1","HDAC6","SIRT2")
-goi <- toupper(goi)
+
 
 if (any(list.files() %in% paste(cancer,"_vital_stat.rds", sep = ""))) {
 
@@ -27,6 +26,8 @@ data <- readRDS(paste(cancer, "_vital_stat.rds", sep = ""))
 prep <- readRDS(paste(cancer, ".rds", sep = ""))
 clinical_data <- colData(prep)
 clinical_data <- as.data.frame(clinical_data)
+clinical_data <- clinical_data[!is.na(clinical_data$vital_status),]
+clinical_data$vital_status <- factor(clinical_data$vita;_status, levels = c("Alive","Dead"))
 rm(prep)
 gc()
 
@@ -36,13 +37,13 @@ gc()
 
 	print(paste(cancer, "DATA AVAILABLE - RUNNING DESEQ", sep = " "))
 	prep <- readRDS(paste(cancer, ".rds", sep = ""))
+	prep <- prep[,!is.na(prep$vital_status)]
+	prep$vital_status <- factor(prep$vital_status, levels = c("Alive","Dead"))
+
 	clinical_data <- colData(prep)
 	clinical_data <- as.data.frame(clinical_data)
-	gc()
 
-	data <- prep[,!is.na(prep$vital_status)]
-	data$vital_status <- factor(data$vital_status, levels = c("Dead","Alive"))
-	data <- DESeqDataSet(data, design = ~ vital_status)
+	data <- DESeqDataSet(prep, design = ~ vital_status)
 	keep <- rowSums(counts(data)) >= 10
 	data <- data[keep,]
 	rm(prep)
@@ -56,29 +57,29 @@ gc()
 	print(paste("STARTING DOWNLOAD",cancer, sep = " - "))
 
 	proj <- paste("TCGA-",cancer,sep = "")
-	data <- GDCquery(project = proj,
+	query <- GDCquery(project = proj,
                       data.category = "Transcriptome Profiling",
                       experimental.strategy = "RNA-Seq",
                       data.type = "Gene Expression Quantification",
                       sample.type = c("Solid Tissue Normal","Primary Tumor"))
 
-	GDCdownload(data, files.per.chunk = 75)
-	prep <- GDCprepare(data)
+	GDCdownload(query, files.per.chunk = 75)
+	prep <- GDCprepare(query)
+	
+	saveRDS(prep, paste(cancer,".rds", sep = ""))
+	prep <- prep[,!is.na(prep$vital_status)]
+	prep$vital_status <- factor(prep$vital_status, levels = c("Alive","Dead"))
 	clinical_data <- colData(prep)
 	clinical_data <- as.data.frame(clinical_data)
-	saveRDS(paste(cancer,".rds", sep = ""))
-	gc()
 	
 	print("RUNNING DESEQ")
-	data <- prep[,!is.na(prep$vital_status)]
-	data$vital_status <- factor(data$vital_status, levels = c("Dead","Alive"))
-	data <- DESeqDataSet(data, design = ~ vital_status)
+	data <- DESeqDataSet(prep, design = ~ vital_status)
 	keep <- rowSums(counts(data)) >= 10
 	data <- data[keep,]
 	rm(prep)
 	gc()
 	data <- DESeq(data)
-	saveRDS(data,paste(cancer, "#DATA TYPE#.rds", sep = ""))
+	saveRDS(data,paste(cancer, "_vital_stat.rds", sep = ""))
 	gc()
 
 }
@@ -91,53 +92,53 @@ res <- as.data.frame(res)
 res$gene_name <- rowRanges(data)[keep,]$gene_name
 res <- res[order(res$gene_name),]
 res$p.signif <- ifelse(res$padj > 0.05, "ns",
-						ifelse(res$padj > 0.01, "*",
-						ifelse(res$padj > 0.001, "**",
-						ifelse(res$padj > 0.0001, "***","****"
-						))))
+			ifelse(res$padj > 0.01, "*",
+			ifelse(res$padj > 0.001, "**",
+			ifelse(res$padj > 0.0001, "***","****"
+			))))
 
 #extract normalised gene counts from data set for plotting
 vsd <- vst(data, blind = F)
-gene.count <- assay(vsd)
+gene_count <- assay(vsd)
 keep <- which(rowRanges(data)$gene_name %in% goi)
 gene_id <- rowRanges(data)$gene_id[keep]
-keep <- which(rownames(gene.count) %in% gene_id)
-gene.count <- gene.count[keep,,drop = T]
-gene.count <- as.data.frame(t(gene.count))
+keep <- which(rownames(gene_count) %in% gene_id)
+gene_count <- gene_count[keep,,drop = T]
+gene_count <- as.data.frame(t(gene_count))
 
 #replace column names from ENSG code to NCBI name
-for(i in 1:length(goi)) {
-  keep <- which(rowRanges(data)$gene_name %in% goi[i])
-  tmp <- which(colnames(gene.count) %in% rowRanges(data)$gene_id[keep])
-  colnames(gene.count)[tmp] <- goi[i]
+for(gene in goi) {
+  keep <- which(rowRanges(data)$gene_name %in% gene)
+  tmp <- which(colnames(gene_count) %in% rowRanges(data)$gene_id[keep])
+  colnames(gene_count)[tmp] <- gene
 }
 
-gene.count$barcode <- rownames(gene.count)
-gene.data <- left_join(clinical_data, gene.count, by = "barcode")
+gene_count$barcode <- rownames(gene_count)
+gene_data <- left_join(clinical_data, gene_count, by = "barcode")
 
-for(i in 1:length(goi)) {
-	tmp <- gene.data[c("vital_status",goi[i])]
-	tmp$gene <- goi[i]
+for(gene in goi) {
+	tmp <- gene_data[c("vital_status",gene)]
+	tmp$gene <- gene
 	colnames(tmp)[2] <- "counts"
-	assign(goi[i], value = tmp)
+	assign(gene, value = tmp)
 	}
 
 tmp <- mget(goi[1:length(goi)])
-vital_vs_tpm <- do.call(rbind, tmp)
-keep <- !is.na(vital_vs_tpm$vital_status) 
-vital_vs_tpm <- vital_vs_tpm[keep,]
+vital_vs_counts <- do.call(rbind, tmp)
+keep <- !is.na(vital_vs_counts$vital_status) 
+vital_vs_counts <- vital_vs_counts[keep,]
 
-stat.test <- compare_means(counts ~ vital_status, data = vital_vs_tpm, group.by = "gene", method = "wilcox")
-stat.test <- as.data.frame(stat.test)
-stat.test$p.signif <- res$p.signif
+stat_test <- compare_means(counts ~ vital_status, data = vital_vs_counts, group.by = "gene", method = "wilcox")
+stat_test <- as.data.frame(stat_test)
+stat_test$p.signif <- res$p.signif
 
-p1 <- ggplot(vital_vs_tpm) +
+p1 <- ggplot(vital_vs_counts) +
   geom_boxplot(aes(x = gene, y = counts, fill = vital_status)) +
-  stat_pvalue_manual(stat.test, x = "gene", label = "p.signif", y.position = max(vital_vs_tpm$counts) + 0.5)
+  stat_pvalue_manual(stat_test, x = "gene", label = "p.signif", y.position = max(vital_vs_counts$counts) + 0.5)
 
 name <- as.character()
-for(i in 1:length(goi)) {
-  name <- paste(name, goi[i], sep = "_")
+for(gene in goi) {
+  name <- paste(name, gene, sep = "_")
 }
 
 ggsave(file = paste(cancer,"_vital_stat_",name,".png", sep = ""), p1)
@@ -151,10 +152,9 @@ cbind.fill <- function(...){
 }
 
 count_csv <- data.frame()
-
-for(i in 1:length(goi)) {
+for(gene in goi) {
   
-  tmp <- get(goi[i])
+  tmp <- get(gene)
   keep <- which(tmp$vital_status %in% "Alive")
   tmp_a <- tmp[keep,]
   keep <- which(tmp$vital_status %in% "Dead")
@@ -162,10 +162,9 @@ for(i in 1:length(goi)) {
   
   count_csv <- cbind.fill(count_csv,tmp_a[,2],tmp_d[,2])
   count_csv <- as.data.frame(count_csv)
-  colnames(count_csv)[c(i*2-1,i*2)] <- c(paste(goi[i],"vst_counts","Alive",sep = "_"),paste(goi[i],"vst_counts","Dead",sep = "_"))
+  colnames(count_csv)[c(i*2-1,i*2)] <- c(paste(gene,"vst_counts","Alive",sep = "_"),paste(gene,"vst_counts","Dead",sep = "_"))
   
 }
 
 count_csv[is.na(count_csv)] <- ""
-
 write.csv(count_csv, file = paste(cancer,"_vital_stat_",name,"_vst_counts.csv", sep = ""), row.names = F)
